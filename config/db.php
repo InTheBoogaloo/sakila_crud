f546fc5c57ef032d7cf4c8599db2026a7e6b8d8d
qwerty <qwerty@void_laptop.localdomain>
2026-01-30 11:20:34 -0600

    se actualiza la contraseña de la bd

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
-$conn = new mysqli("localhost", "sakila_admin", "123", "sakila");
-//$conn = new mysqli("localhost", "db_22030618", "22030618", "sakila");
+//$conn = new mysqli("localhost", "sakila_admin", "123", "sakila");
+$conn = new mysqli("localhost", "db_22030618", "22030618", "sakila");
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
